e40f3281ec6bdbf3374bcb64d05ad04df36dd058
Daylin Morgan <daylinmorgan@gmail.com>
2021-11-04 09:14:54 -0500

    style: black formatting

--- seqdat/cli.py
+++ seqdat/cli.py
@@ -1,7 +1,7 @@
 import click
-from click_help_colors import HelpColorsGroup, HelpColorsCommand
-from rich.prompt import Prompt
+from click_help_colors import HelpColorsCommand, HelpColorsGroup
 from rich.markdown import Markdown
+from rich.prompt import Prompt
 
 from .__init__ import version
 from .console import console
@@ -17,13 +17,13 @@ def cli():
 
 
 @cli.command()
-@click.option("-u","--user", help="name of user who submitted job")
-@click.option("-s","--sample", help="sample id")
+@click.option("-u", "--user", help="name of user who submitted job")
+@click.option("-s", "--sample", help="sample id")
 def query(user: str, sample: str) -> None:
-    """Query for the data""" 
-    
+    """Query for the data"""
+
     console.print("Searching the data...")
-    
+
     if user:
         console.print(f"Searching for user: [cyan]{user}")
     if sample:
@@ -34,14 +34,14 @@ def query(user: str, sample: str) -> None:
 def init():
     """Interactively generate a new project"""
     console.print("Making a project folder...")
-    name = Prompt.ask("User")
+    name = Prompt.ask("User")  # noqa
 
 
 @cli.command()
 @click.argument("project")
-def info(project:str):
+def info(project: str):
     """Render the individual project's README"""
-    MARKDOWN = f"""
+    MARKDOWN = fr"""
 # {project}
 
 - User: Daylin
@@ -50,29 +50,30 @@ def info(project:str):
 
 ## Additional Info
 
-Here is other project specific info. 
+Here is other project specific info.
 
 Like that this was MDA-MB-231 cells
-""" 
+"""
 
     from rich.panel import Panel
+
     md = Markdown(MARKDOWN)
-    console.print("[blue] Job Info Sheet", justify='center',width=80)
-    console.print(Panel(md),width=80)
+    console.print("[blue] Job Info Sheet", justify="center", width=80)
+    console.print(Panel(md), width=80)
 
 
 def main():
 
-    head="""
-     __  _  _   _      ___ 
-    (_  |_ / \ | \  /\  |  
-    __) |_ \_X |_/ /--\ |  
-                          
+    head = r"""
+     __  _  _   _      ___
+    (_  |_ / \ | \  /\  |
+    __) |_ \_X |_/ /--\ |
+
     """
-    console.print(head, style='blue',highlight=False)
+    console.print(head, style="blue", highlight=False)
     console.print("Brock lab [green bold]SEQ[/]uencing [green bold]DAT[/]a manager.\n")
     # console.print(f"version: {version}\n")
-    
+
     cli()
 
 
